2b5de915be4ab2985aa9e767537f253d891dc104
Erez Wiezel <99121505+erezviz@users.noreply.github.com>
2022-07-26 13:13:35 +0300

    "Vercel deployment"

---
+++ pages/bobo/bobo.js
@@ -0,0 +1,15 @@
+import Head from "next/head"
+import Layout from "../../components/layout"
+
+export default function Bobo() {
+    return (
+
+        <Layout>
+            <Head>
+                <title>Hi Bobo</title>
+            </Head>
+            <h1>I Am BOBO</h1>
+        </Layout>
+
+    )
+}
